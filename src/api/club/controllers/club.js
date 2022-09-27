d5df06cd2f56020e8eaa2904c2fdecb9e8a9b1e0
maybe vaggy <sarvagya6@gmail.com>
2022-09-26 20:13:40 -0400

    superCreate controller for club population

--- src/api/club/controllers/club.js
+++ src/api/club/controllers/club.js
@@ -5,5 +5,44 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
+const axios = require('axios');
 
-module.exports = createCoreController('api::club.club');
+module.exports = createCoreController("api::club.club", ({ strapi }) => ({
+  async superCreate(ctx) {
+    try {
+      const response = await axios.get(
+        "https://rowan.campuslabs.com/engage/api/discovery/search/organizations?orderBy[0]=UpperName%20asc&top=500"
+      );
+      const data = response.data.value;
+      console.log(data);
+      //map the data to the club model using strapi create
+      data.forEach(async (club) => {
+        try {
+          const clubData = {
+            name: club.Name,
+            acronym: club.ShortName,
+            websiteKey: club.WebsiteKey,
+            uid: club.Id,
+          };
+
+          ctx.request.body.data = {
+            ...ctx.request.body.data,
+            ...clubData,
+          };
+
+          await super.create(ctx);
+          return data;
+
+        } catch (error) {
+          console.error(error);
+          //return 400 error
+          return ctx.badRequest(null, error);
+        }
+      });
+
+
+    } catch (error) {
+      console.error(error);
+    }
+  },
+}));
